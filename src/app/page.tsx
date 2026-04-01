6dcb0df85b1a876935c79d258c283d34fd35c7d2
Abdul Rahim <chaudharyabdulrahimamir@gmail.com>
2026-04-02 01:27:17 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import MatchaScrollScene from "@/components/MatchaScrollScene";
+
+export default function Home() {
+  return (
+    <main className="bg-[#050505] min-h-screen selection:bg-green-500/30">
+      <MatchaScrollScene />
+    </main>
+  );
+}
